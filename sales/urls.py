34ae1ae7edaa531e83935973c4c62edda819b4b2
Marco Polver <m.polver@studenti.unibg.it>
2019-07-31 17:06:22 +0200

    Modificata la schermata con i libri consigliati, inserite pagine rudimentali per profilo utente e articoli.

--- sales/urls.py
+++ sales/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup'),
-    path('<str:username>', views.first_page),
-    path('ads/<str:ad_pk>', views.ad_details),
-    #path('titles/<str:title_pk>', views.title_details),
+    path('', views.home, name='home'),                      #Home page
+    path('signup/', views.signup, name='signup'),           #Registrazione
+    path('<str:username>/suggested', views.first_page),     #Consigli intelligenti
+    path('<str:username>', views.profile),                  #Profilo utente
+    path('ads/<str:ad_pk>', views.ad_details),              #Annuncio
+    #path('titles/<str:title_pk>', views.title_details),    #Titolo
 ]
